5ca408f00e5d2027be63516b7ea48237a9eaa201
cuzzing <donganqi@foxmail.com>
2016-07-17 09:53:56 +0800

    delete

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding=utf-8
-
-import requests
-# import json
-r = requests.get('http://movie.douban.com/top250/', headers={
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'})
-print(r.url)
